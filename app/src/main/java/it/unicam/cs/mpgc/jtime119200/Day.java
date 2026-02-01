0ac566645010d33499bb7c824c5e77dd20038a5b
AngeloSab <angelo.sabetta@studenti.unicam.it>
2026-02-01 19:00:31 +0100

    git to see try

--- app/src/main/java/it/unicam/cs/mpgc/jtime119200/Day.java
+++ app/src/main/java/it/unicam/cs/mpgc/jtime119200/Day.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+//git to see
 /**
  * Represents a day in the calendar. Each day can contain multiple activities.
  */
